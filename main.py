f4b1164991c0c1ddfa8d57cb61973121aa412d2e
projectormato <k.keita811.anuy@gmail.com>
2018-10-16 00:44:20 +0900

    draw text in canvas.

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ def saveimage():
 
     return make_response(img_name, 200)
 
-line_bot_api = LineBotApi(os.environ.get('ACCESS_TOKEN'))
-handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
+# line_bot_api = LineBotApi(os.environ.get('ACCESS_TOKEN'))
+# handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -74,7 +74,7 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+# @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == '決断':
         line_bot_api.reply_message(
@@ -86,4 +86,5 @@ def handle_message(event):
             TextSendMessage(text=os.environ.get('SKETCH_LIFF_APP')))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
+    app.run()
+    # app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
