6817f66f5cd5846f6e0fc1c86dc72f69b0759ab6
projectormato <k.keita811.anuy@gmail.com>
2018-10-15 23:39:36 +0900

    Implement sketch page.

--- main.py
+++ main.py
@@ -16,17 +16,42 @@ from PIL import Image
 import warnings
 warnings.simplefilter('error', Image.DecompressionBombWarning)
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='imgs')
 bootstrap = Bootstrap(app)
 
+# 決断LIFFのurl
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+# お絵かきLIFFのurl
 @app.route("/sketch")
 def sketch():
     return render_template('sketch.html')
 
+# お絵かき保存のurl
+@app.route('/saveimage', methods=['POST'])
+def saveimage():
+    event = request.form.to_dict()
+
+    dir_name = 'imgs'
+    img_name = uuid.uuid4().hex
+
+    # Saving image in the 'imgs' folder temporarily. Should be deleted after a certain period of time
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    with open(os.path.join(dir_name, '{}.jpg'.format(img_name)), 'wb') as img:
+        img.write(base64.b64decode(event['image'].split(",")[1]))
+
+    original = Image.open(os.path.join(dir_name, '{}.jpg'.format(img_name)))
+    # Needs simple validation of format for security since Pillow supports various type of Images
+    if(original.format != 'JPEG'):
+        return make_response('Unsupported image type.', 400)
+
+    original.thumbnail((240, 240), Image.ANTIALIAS)
+    original.save(os.path.join(dir_name, '{}_240.jpg'.format(img_name)), 'JPEG')
+
+    return make_response(img_name, 200)
 
 line_bot_api = LineBotApi(os.environ.get('ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
